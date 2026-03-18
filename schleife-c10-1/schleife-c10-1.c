68632d7c63d1a1392bffbee7e49aa50ede6cb013
KB <9115824@gmail.com>
2026-03-18 18:34:51 +0100

    Add schleife-c10-1

---
+++ schleife-c10-1/schleife-c10-1.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void print10bis1() {
+    for(int i=10; i >=1; i--){
+        printf("%d\n", i);
+
+    }
+
+
+}
+
+int main () { 
+print10bis1();
+    return 0;
+}
